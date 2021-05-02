5235825092fc823ea86687266e751c8dd88719eb
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-05-02 17:53:28 -0300

    adding user identification screen, confirmation screen, navigation and ux improvements

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   SafeAreaView,
+  View,
   Text,
   Image,
   TouchableOpacity,
   StyleSheet,
+  Dimensions,
 } from "react-native";
-
-import { Button } from "../components/Button";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/core";
 
 import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 import wateringImg from "../assets/watering.png";
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate("UserIdentification");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas {"\n"}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de{"\n"}
+          forma fácil
+        </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
 
-      <Button title=">" />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.8}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -37,24 +54,43 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontFamily: fonts.heading,
+    fontSize: 28,
     fontWeight: "bold",
     textAlign: "center",
     color: colors.heading,
     marginTop: 38,
+    lineHeight: 34,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
   },
   subtitle: {
+    fontFamily: fonts.text,
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
   },
-  image: {
-    width: 292,
-    height: 284,
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
   },
 });
